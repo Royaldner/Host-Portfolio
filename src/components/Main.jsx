b871f23b3523c937fed2b7ad0282616c6a9d9bc9
Roy Aldner Labado <144700694+Royaldner@users.noreply.github.com>
2024-01-21 19:50:43 -0500

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -75,7 +75,7 @@ function Main() {
                 whileTap={{ scale: 0.95 }}
                 variants={Variants}
               >
-                Recent Projects 23
+                Recent Projects 234
               </motion.button>
             </a>
             <a href="#services">
