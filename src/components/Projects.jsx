f095fc743ee6945c2febf3f8ad3d5442d482fd41
Roy Aldner Labado <144700694+Royaldner@users.noreply.github.com>
2024-01-21 21:15:18 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,9 +23,8 @@ function Projects(props) {
         </motion.h1>
         <motion.h2 className="text-xl font-thin">{props.description}</motion.h2>
         <div className="text-lime-300 font-semibold"> {props.languages}</div>
-        <motion.a href={props.link}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}>
+        <motion.a href={props.link} className="z-[99]"
+            >
           <motion.button
             className=" w-48 h-14 rounded-md bg-lime-300 text-2xl z-[99]"
             whileHover={{ scale: 1.05 }}
